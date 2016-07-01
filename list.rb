88a9121e6b658e9f447a98b79f775537ba9e0d48
Josh Nixon <seriousfools@gmail.com>
2016-07-01 17:08:38 -0500

    answers

--- list.rb
+++ list.rb
@@ -11,11 +11,60 @@ class List
 
   def incomplete_task_names
     task_names = []
+
     @tasks.each do |task|
       unless task.complete
         task_names << task.name
       end
     end
-    return task_names
+    
+    task_names
+  end
+
+  def number_of_incomplete_tasks
+    counter = 0
+
+    @tasks.each do |task|
+      if task.complete == false
+        counter += 1
+      end
+    end
+
+    counter
+  end
+
+  def delete_complete_tasks
+    complete_tasks = []
+
+    @tasks.each do |task|
+      if task.complete
+        complete_tasks << task
+      end
+    end
+
+    complete_tasks.each do |task|
+      @tasks.delete(task)
+    end
+  end
+
+  def sort_by_priority
+    sorted_array_low_to_high = @tasks.sort_by { |task| task.priority }
+    sorted_array_high_to_low = sorted_array_low_to_high.reverse
+    sorted_names = []
+
+    sorted_array_high_to_low.each do |task|
+      sorted_names << task.name
+    end
+
+    sorted_names
   end
 end
+
+
+
+
+
+
+
+
+
--- task.rb
+++ task.rb
@@ -1,12 +1,32 @@
 class Task
-  attr_reader :name, :complete
 
-  def initialize(name)
+  def initialize(name, priority=5)
     @name = name
     @complete = false
+    @priority = priority
+  end
+
+  def name
+    @name
+  end
+
+  def complete
+    @complete
+  end
+
+  def priority
+    @priority
+  end
+
+  def priority=(new_priority)
+    @priority = new_priority
   end
 
   def complete!
     @complete = true
+  end
+
+  def toggle_complete!
+    @complete = !@complete
   end
 end
